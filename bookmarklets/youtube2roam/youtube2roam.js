64222cc3ea8701c90788d498804c17abc4ceb262
SERIZAWA <78351950+serizawa-jp@users.noreply.github.com>
2021-02-12 21:46:37 +0900

    add youtube transcript bookmarklet (#7)

---
+++ bookmarklets/youtube2roam/youtube2roam.js
@@ -0,0 +1,48 @@
+const fetchTranscriptElment = () => document.querySelectorAll("ytd-transcript-renderer ytd-transcript-body-renderer>div");
+
+const copyTranscript = (transcriptElement) => {
+    const transcriptBody = Array.from(transcriptElement);
+
+    const transcripts = transcriptBody.map(el => {
+        const row = Array.from(el.querySelectorAll('div')).slice(0, 2);
+        const timestamp = row[0].textContent.trim();
+        const transcript = row[1].textContent.trim();
+        return { timestamp, transcript };
+    });
+
+    let res = "";
+
+    transcripts.forEach(t => {
+        res += [`${t.timestamp}`, `  ${t.transcript}\n`].join("\n");
+    });
+
+    navigator.clipboard.writeText(res);
+};
+
+let transcriptElement = fetchTranscriptElment();
+
+if (transcriptElement.length !== 0) {
+    copyTranscript(transcriptElement);
+    return;
+}
+
+// click menu button
+Array.from(document.querySelectorAll('.ytd-video-primary-info-renderer button')).slice(-1)[0].click()
+
+// click open a transcript button
+const t = setInterval(() => {
+    try {
+        Array.from(document.querySelectorAll('ytd-menu-popup-renderer paper-listbox ytd-menu-service-item-renderer')).slice(-1)[0].click()
+    } catch (e) {
+        console.log('WARN: failed to click button opening a transcript');
+        return;
+    }
+    clearInterval(t);
+
+    const t2 = setInterval(() => {
+        const transcriptElement = fetchTranscriptElment();
+        if (transcriptElement.length === 0) return;
+        copyTranscript(transcriptElement);
+        clearInterval(t2);
+    }, 100);
+}, 100);
