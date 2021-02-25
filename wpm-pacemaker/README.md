f8a55703e66e7b0745c92e25c6cdcae31d33aa89
SERIZAWA <serizawa@hey.com>
2021-02-25 19:13:19 +0900

    init

---
+++ wpm-pacemaker/README.md
@@ -0,0 +1,28 @@
+# wpm-pacemaker
+
+## Installation
+
+To install, do the same thing you do for any roam/js script.
+
+1. Create page in Roam (if not already present) called [[roam/js]]
+
+1. If you previously installed this script by copying from a GitHub Gist, remove it from [[roam/js]] now.
+
+1. Create a new block on this page and enter: ```
+
+1. Nest under that block a Code Block
+
+1. Make sure the code language is set as JavaScript
+
+1. Paste the following into the new Code Block
+
+```javascript
+ window.roamkit.wpmPacemaker = {
+   wpm: 200,
+ };
+
+ var s = document.createElement("script");
+ s.type = "text/javascript";
+ s.src = "https://cdn.jsdelivr.net/gh/serizawa-jp/roamkit@main/wpm-pacemaker/dist/wpm-pacemaker.min.js";
+ document.getElementsByTagName("head")[0].appendChild(s);
+```
---
+++ wpm-pacemaker/src/App.ts
@@ -0,0 +1,38 @@
+import Countable from 'countable';
+import lumin from 'lumin';
+
+const defaultConfig = {
+    wpm: 200
+};
+
+const config = {...defaultConfig, ...window?.roamkit?.wpmPacemaker};
+
+const wpm = config.wpm;
+
+function startPacemaker(button, children, wordsCount, wpm) {
+    const l = lumin(children);
+    const ms = (wordsCount / wpm) * 60000; // min -> milisec
+
+    console.log(`Words: ${wordsCount}`);
+    console.log(`WPM: ${wpm}`);
+    console.log(`ms: ${ms}`);
+
+    button.addEventListener('click', e => {
+        l.clear();
+        l.start(ms);
+    });
+}
+
+const addEventListener = (button) => {
+    const parent = button.closest(".rm-block-main");
+    const children = parent.nextElementSibling;
+
+    Countable.count(children, c => startPacemaker(button, children, c.words, wpm));
+};
+
+const buttonName = "wpm";
+const buttons = Array.from(document.querySelectorAll(".roam-article button")).filter(e => e.textContent === buttonName);
+
+buttons.forEach(b => {
+    addEventListener(b);
+});
